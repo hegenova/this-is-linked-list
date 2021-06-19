6669e5fc659071cd3ca066d9abe93ad2d581bebc
wawa <handofchaos93@gmail.com>
2021-06-19 20:31:08 +0700

    Initial Commit

---
+++ this is linked list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  this is linked list
+//
+//  Created by Riley Spencer on 19/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
